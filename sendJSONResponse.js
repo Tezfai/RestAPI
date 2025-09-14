deeb8ec9dcf13e46aedd4dc78cffd77f693d0c76
Tezfai <etesfai@ualberta.ca>
2025-09-13 23:37:57 -0600

    Delete sendJSONResponse.js

--- sendJSONResponse.js
+++
@@ -1,7 +0,0 @@
-export const sendJSONResponse = (res, statusCode, payload) => {
-    res.setHeader('Content-Type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*') // allow access from any domain
-    res.setHeader('Access-Control-Allow-Methods', 'GET') // GET method only
-    res.statusCode = statusCode
-    res.end(JSON.stringify(payload))
-}
